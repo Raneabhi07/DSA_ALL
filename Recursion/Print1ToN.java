6d8c979d3d58338298abcf69f2083d3f1fc87ac2
Abhijeet Rane <83912624+Raneabhi07@users.noreply.github.com>
2023-08-06 05:55:44 +0530

    Update Print1ToN.java

--- Recursion/Print1ToN.java
+++ Recursion/Print1ToN.java
@@ -1,1 +1,17 @@
+package Recursions;
+
+public class PrintIncrease {
+    public static void PIncrese(int n){
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        PIncrese(n-1);
+        System.out.println(n + " ");
+    }
+    public static void main(String[] args) {
+        int n =10;
+        PIncrese(n);
+    }
+}
 
